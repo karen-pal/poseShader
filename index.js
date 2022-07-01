d5cd477358fe96c5a1b2076dcb4e3923dd678c7d
Karen Palacio <karen.palacio.1994@gmail.com>
2022-07-01 00:41:36 -0300

    nuevo gesto: taparse la nariz con las manos

--- index.js
+++ index.js
@@ -35,7 +35,27 @@ function onResults(results) {
   //canvasCtx.fillStyle = '#00FF00';
   canvasCtx.drawImage(videoElement,0,0,canvasElement.width, canvasElement.height);
 
-
+///aver
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+     // Set a rectangle the same size as the image.
+    //aver
   //// Only overwrite missing pixels.
   //canvasCtx.globalCompositeOperation = 'destination-atop';
   //canvasCtx.drawImage(
@@ -48,18 +68,35 @@ function onResults(results) {
                 {color: '#FF0000', lineWidth: 2});
   let right_hand = results.poseLandmarks[16];
   let left_hand = results.poseLandmarks[15];
+  let right_shoulder = results.poseLandmarks[12];
+  let left_shoulder = results.poseLandmarks[11];
+  let nose = results.poseLandmarks[0];
+
     //console.log(left_hand);
   //uLeft.set(left_hand.x, left_hand.y);
+  function getDistance (x1,y1,x2,y2){
+    var a = x1 - x2;
+    var b = y1 - y2;
+
+    return Math.sqrt( a*a + b*b );
+
+  }
   uMouse.set(right_hand.x*2000, right_hand.y*-2500);
     let distance = Math.abs(Math.abs(right_hand.x)-Math.abs(left_hand.x))
     let hasInfo = (left_hand.x>0 && right_hand.x>0);
     //console.log(distance);
+    let rightHandToShoulder = getDistance(right_shoulder.x,right_shoulder.y, right_hand.x, right_hand.y);
+    let rightHandToNose = getDistance(nose.x,nose.y, right_hand.x, right_hand.y);
+    let leftHandToShoulder =getDistance(left_shoulder.x,left_shoulder.y, left_hand.x, left_hand.y)
+    let leftHandToNose =getDistance(nose.x,nose.y, left_hand.x, left_hand.y)
     if ( hasInfo && distance<.03){
-        console.log("touch ");
         uTouch.set(1.);
         uLeft.set(distance,distance);
         //setTimeout(function() { uTouch.set(0.);console.log("timeout!")}, 2000);
     }
+    uHandsShoulders.set(rightHandToShoulder, leftHandToShoulder);
+    uHandsNose.set(rightHandToNose, leftHandToNose);
+    console.log(rightHandToNose);
     if (hasInfo && distance>.2){
         uTouch.set(0.)
     }
